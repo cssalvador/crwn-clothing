d353a5cd6938aafebf9c5745250805ae90ca3238
Christian Salvador <christiansalvador6@gmail.com>
2021-01-13 11:22:52 +0100

    79. Shop Page init

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -49,7 +49,7 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size, linkUrl,  }) => (
+        {this.state.sections.map(({ title, imageUrl, id, size, linkUrl  }) => (
           <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} linkUrl={linkUrl} />
         ))}
       </div>
